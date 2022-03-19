3789754bba9a49e00d10e19b20f8d66f12ef671c
shahriyar-tanna <80189281+shahriyar-tanna@users.noreply.github.com>
2022-03-20 01:37:32 +0600

    Add files via upload

---
+++ function.php
@@ -0,0 +1,14 @@
+<?php
+
+function getAge($name, $year){
+
+    $get_age = 2022 - $year;
+
+    return "Hi {$name}, You are {$year} years old.";
+
+}
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+include_once "./function.php";
+
+echo getAge("Shah", 1997);
+
+?>
